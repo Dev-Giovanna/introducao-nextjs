670c0d639fb787a8be8ef9dbd293b9ae7aae2da7
Giovanna <gg.aventureira@gmail.com>
2025-05-27 19:19:24 -0300

    atv01

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,14 @@
+export default function Welcome(){
+    return (
+        <div>
+            <h1> Bem-vind(a) à Home </h1>
+            <p>
+                Esta é minha primeira página
+                em next.js
+            </p>
+
+            <button><a href="/about"> Clique </a></button>
+
+        </div>
+    )
+}
